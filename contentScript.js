11989c3d0d8a2e8528881ae834fb1944a4caa2d9
dreemhai <dreemhai.wk@gmail.com>
2023-08-11 17:01:08 +0200

    ajout button on off

--- contentScript.js
+++ contentScript.js
@@ -1,22 +1,51 @@
 function countTokens(str) {
-    // Cette fonction est une simplification. Pour une comptabilité exacte des tokens,
-    // vous devrez probablement utiliser une bibliothèque ou un algorithme spécifique.
     return str.split(/\s+/).length;
 }
 
-let inputField = document.querySelector('textarea'); // Remplacez ceci par le sélecteur approprié pour le champ d'input de ChatGPT
-if (inputField) {
-    let tokenCounterDiv = document.createElement('div');
-    tokenCounterDiv.style.position = 'absolute';
-    tokenCounterDiv.style.top = '-30px'; // Ajustez selon l'emplacement souhaité
-    tokenCounterDiv.style.left = '0';
-    tokenCounterDiv.style.backgroundColor = '#343541';
-    tokenCounterDiv.style.padding = '0px';
-    tokenCounterDiv.textContent = 'Tokens: 0';
+let tokenCounterDiv = null;
+let toggleButton = null;
+let inputField = document.querySelector('textarea');
 
-    inputField.parentElement.appendChild(tokenCounterDiv);
+function setupTokenCounter() {
+    if (inputField) {
+        tokenCounterDiv = document.createElement('div');
+        tokenCounterDiv.style.position = 'absolute';
+        tokenCounterDiv.style.top = '-30px';
+        tokenCounterDiv.style.left = '0'; // Positionnez le compteur à gauche
+        tokenCounterDiv.style.padding = '0px';
+        tokenCounterDiv.style.color = 'white';
+        tokenCounterDiv.textContent = 'Tokens: 0';
 
-    inputField.addEventListener('input', function() {
-        tokenCounterDiv.textContent = 'Tokens: ' + countTokens(inputField.value);
-    });
+        toggleButton = document.createElement('button');
+        toggleButton.type = 'button'; // Ceci empêche le bouton de soumettre le formulaire
+        toggleButton.textContent = 'OFF';
+        toggleButton.style.backgroundColor = '#FF4136'; // Rouge pour OFF
+        toggleButton.style.border = 'none';
+        toggleButton.style.color = 'white';
+        toggleButton.style.marginLeft = '10px';
+        toggleButton.style.cursor = 'pointer';
+        toggleButton.style.borderRadius = '5px';
+
+        toggleButton.addEventListener('click', function() {
+            if (toggleButton.textContent === 'OFF') {
+                toggleButton.textContent = 'ON';
+                toggleButton.style.backgroundColor = '#2ECC40'; // Vert pour ON
+                inputField.addEventListener('input', updateTokenCount);
+            } else {
+                toggleButton.textContent = 'OFF';
+                toggleButton.style.backgroundColor = '#FF4136'; // Rouge pour OFF
+                inputField.removeEventListener('input', updateTokenCount);
+            }
+        });
+
+        inputField.parentElement.appendChild(tokenCounterDiv);
+        tokenCounterDiv.appendChild(toggleButton);
+    }
 }
+
+function updateTokenCount() {
+    tokenCounterDiv.textContent = 'Tokens: ' + countTokens(inputField.value);
+    tokenCounterDiv.appendChild(toggleButton); // Gardez le bouton à droite du texte du compteur
+}
+
+setupTokenCounter();
