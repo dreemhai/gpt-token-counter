3246756477b293ac6a94f78002fb90dbc6ced918
dreemhai <dreemhai.wk@gmail.com>
2023-08-11 23:12:19 +0200

    modif html

--- popup.js
+++ popup.js
@@ -1,23 +1,9 @@
-const checkbox = document.getElementById('alwaysOnCheckbox');
 const toggleButton = document.getElementById('toggleButton');
 // Charge la valeur actuelle de la case à cocher depuis le stockage local
 browser.storage.local.get('isButtonOn').then(result => {
     if (result.isButtonOn) {
         toggleButton.classList.add('on');
         toggleButton.textContent = 'ON';
-    }
-});
-
-// Écoute les changements de la case à cocher
-checkbox.addEventListener('change', () => {
-    if (checkbox.checked) {
-        // Stocke l'état de la case à cocher comme cochée
-        browser.storage.local.set({ isCheck: true });
-        browser.runtime.sendMessage({ action: 'activateFunction' });
-    } else {
-        // Stocke l'état de la case à cocher comme non cochée
-        browser.storage.local.set({ isCheck: false });
-        browser.runtime.sendMessage({ action: 'activateFunction' });
     }
 });
 
