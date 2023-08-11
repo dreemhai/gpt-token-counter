85f95160044ef54b4556d2b17bbc15ffe301de8d
dreemhai <dreemhai.wk@gmail.com>
2023-08-11 22:51:58 +0200

    a corriger

--- contentScript.js
+++ contentScript.js
@@ -41,19 +41,6 @@ function setupTokenCounter() {
         tokenCounterDiv.textContent = 'Tokens: 0';
 
         toggleButton = document.createElement('button');
-        toggleButton.type = 'button'; // Ceci empêche le bouton de soumettre le formulaire
-        toggleButton.textContent = 'OFF';
-        toggleButton.style.backgroundColor = '#FF4136'; 
-        toggleButton.style.border = 'none';
-        toggleButton.style.color = 'white';
-        toggleButton.style.marginLeft = '10px';
-        toggleButton.style.cursor = 'pointer';
-        toggleButton.style.borderRadius = '5px';
-        toggleButton.style.width = '3em';       
-        toggleButton.style.textAlign = 'center';  
-        toggleButton.style.lineHeight = '1.5';     
-
-
         toggleButton.addEventListener('click', function() {
             if (toggleButton.textContent === 'OFF') {
                 toggleButton.textContent = 'ON';
