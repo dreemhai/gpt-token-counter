f09713a0b9b6fffbf9f925a98dce651f43dbb54a
dreemhai <dreemhai.wk@gmail.com>
2023-08-11 17:32:43 +0200

    ajout css

--- contentScript.js
+++ contentScript.js
@@ -11,7 +11,7 @@ function setupTokenCounter() {
         tokenCounterDiv = document.createElement('div');
         tokenCounterDiv.style.position = 'absolute';
         tokenCounterDiv.style.top = '-30px';
-        tokenCounterDiv.style.left = '0'; // Positionnez le compteur à gauche
+        tokenCounterDiv.style.left = '0'; 
         tokenCounterDiv.style.padding = '0px';
         tokenCounterDiv.style.color = 'white';
         tokenCounterDiv.textContent = 'Tokens: 0';
@@ -19,15 +19,15 @@ function setupTokenCounter() {
         toggleButton = document.createElement('button');
         toggleButton.type = 'button'; // Ceci empêche le bouton de soumettre le formulaire
         toggleButton.textContent = 'OFF';
-        toggleButton.style.backgroundColor = '#FF4136'; // Rouge pour OFF
+        toggleButton.style.backgroundColor = '#FF4136'; 
         toggleButton.style.border = 'none';
         toggleButton.style.color = 'white';
         toggleButton.style.marginLeft = '10px';
         toggleButton.style.cursor = 'pointer';
         toggleButton.style.borderRadius = '5px';
-        toggleButton.style.width = '3em';        // Largeur fixe basée sur le contenu le plus large
-        toggleButton.style.textAlign = 'center';   // Centre le texte horizontalement
-        toggleButton.style.lineHeight = '1.5';     // Pour le centrage vertical
+        toggleButton.style.width = '3em';       
+        toggleButton.style.textAlign = 'center';  
+        toggleButton.style.lineHeight = '1.5';     
 
 
         toggleButton.addEventListener('click', function() {
@@ -49,7 +49,7 @@ function setupTokenCounter() {
 
 function updateTokenCount() {
     tokenCounterDiv.textContent = 'Tokens: ' + countTokens(inputField.value);
-    tokenCounterDiv.appendChild(toggleButton); // Gardez le bouton à droite du texte du compteur
+    tokenCounterDiv.appendChild(toggleButton); 
 }
 
 setupTokenCounter();
